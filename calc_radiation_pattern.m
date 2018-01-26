31d782579beb55271ed928e7e014e283e42e092f
VasilevN <vasilevnikitad@yandex.ru>
2018-01-26 21:11:55 +0000

    ИЗИМЕНЕНИЯ:     1. Изменен способ вывода информации. Теперь стандартный вывод     Matlab`а дублируется в указанный файл.     2. Промоделированы распределения для 10000-ой и 40000-ой зоны.     3. Проведены измерения зависимости времени построения и точности     подсчета интеграла от phi_cnt и rho_cnt.

--- calc_radiation_pattern.m
+++ calc_radiation_pattern.m
@@ -18,38 +18,22 @@ function RP_o = calc_radiation_pattern( ...
         RP_o = -1;
         error('wrong input pattern parameters');
     end
-    Rzone_m       = @(n) sqrt( n*focus_dist_m*lambda_m + (n*lambda_m/2)^2 );
-    tic %!!
+    Rzone_m      = @(n) sqrt( n*focus_dist_m*lambda_m + (n*lambda_m/2)^2 );
     Router_m     = Rzone_m(zone_number);
     Rinner_m     = Rzone_m(zone_number-1);
     phi_step_rad = 2*pi/phi_cnt;
     rho_step_m   = (Router_m-Rinner_m)/rho_cnt;
     size_x       = floor( (  pattern_max_x_m - pattern_min_x_m )/pattern_step_x_m ); 
     size_y       = floor( (  pattern_max_y_m - pattern_min_y_m )/pattern_step_y_m ); 
-    fprintf('=========================\n'               );
-    fprintf('_________________________\n'               );
-    fprintf('zone_number      = %d\n', zone_number      );    
-    fprintf('lambda_m         = %f\n', lambda_m         );    
-    fprintf('focus_dist_m     = %f\n', focus_dist_m     );    
-    fprintf('phi_cnt          = %d\n', phi_cnt          );    
-    fprintf('rho_cnt          = %d\n', rho_cnt          );    
-    fprintf('pattern_min_x_m  = %f\n', pattern_min_x_m  );  
-    fprintf('pattern_max_x_m  = %f\n', pattern_max_x_m  );  
-    fprintf('pattern_min_y_m  = %f\n', pattern_min_y_m  );    
-    fprintf('pattern_max_y_m  = %f\n', pattern_max_y_m  );    
-    fprintf('pattern_step_x_m = %f\n', pattern_step_x_m );    
-    fprintf('pattern_step_y_m = %f\n', pattern_step_y_m );    
-    fprintf('_________________________\n'               );
-    fprintf('Router_m         = %f\n', Router_m         );    
-    fprintf('Rinner_m         = %f\n', Rinner_m         );    
-    fprintf('phi_step_rad     = %f\n', phi_step_rad     );   
-    fprintf('rho_step_m       = %f\n', rho_step_m       );   
-    fprintf('size_x           = %d\n', size_x           );    
-    fprintf('size_y           = %d\n', size_y           );    
-    fprintf('_________________________\n'               );
-    fprintf('Calculating radiation pattern...');
     RP = zeros(size_y, size_x);
-    
+    fprintf( '____________________________\n'               );
+    fprintf( 'zone_number      = %d\n',         zone_number      );    
+    fprintf( 'Router_m         = %f\n',         Router_m         );    
+    fprintf( 'Rinner_m         = %f\n',         Rinner_m         );    
+    fprintf( 'phi_step_rad     = %f\n',         phi_step_rad     );   
+    fprintf( 'rho_step_m       = %f\n',         rho_step_m       );   
+    fprintf( 'size_x           = %d\n',         size_x           );    
+    fprintf( 'size_y           = %d\n',         size_y           );    
     rho_min_m = Rinner_m;
     rho_max_m = Router_m;
     for x_i = 1:size_x
@@ -59,7 +43,5 @@ function RP_o = calc_radiation_pattern( ...
             RP(y_i, x_i) = calc_dot( x_m, y_m, zone_number, lambda_m, focus_dist_m, rho_min_m, rho_max_m, phi_step_rad, rho_step_m, phi_cnt, rho_cnt);
         end
     end
-    fprintf(' Done!\n');
-    toc;
     RP_o = RP;
 end
--- main.m
+++ main.m
@@ -1,4 +1,6 @@
-zone_number      = 5;
+
+log_filename     = './output_files/distributions/out_n_40000.txt';
+zone_number      = 40000;
 lambda_m         = 1e-6;
 focus_dist_m     = 50;
 phi_cnt          = 20;
@@ -12,7 +14,26 @@ pattern_min_y_m  = -5*L/2;
 pattern_max_y_m  =  5*L/2;
 pattern_step_x_m =  5*L/60;
 pattern_step_y_m =  5*L/60; 
+
+delete(log_filename);
+diary(log_filename);
+diary on;
 %______________________________________________________
+fprintf( '=============================\n'             );
+fprintf( '    %s\n'               ,   datetime('now')  );
+fprintf( '=============================\n'             );
+fprintf( 'lambda_m         = %f\n',   lambda_m         );    
+fprintf( 'focus_dist_m     = %f\n',   focus_dist_m     );    
+fprintf( 'phi_cnt          = %d\n',   phi_cnt          );    
+fprintf( 'rho_cnt          = %d\n',   rho_cnt          );    
+fprintf( 'pattern_min_x_m  = %f\n',   pattern_min_x_m  );  
+fprintf( 'pattern_max_x_m  = %f\n',   pattern_max_x_m  );  
+fprintf( 'pattern_min_y_m  = %f\n',   pattern_min_y_m  );    
+fprintf( 'pattern_max_y_m  = %f\n',   pattern_max_y_m  );    
+fprintf( 'pattern_step_x_m = %f\n',   pattern_step_x_m );    
+fprintf( 'pattern_step_y_m = %f\n',   pattern_step_y_m );    
+fprintf( '=============================\n'             );
+tic %!!
 U = calc_radiation_pattern( ...
             zone_number,      ... 
             lambda_m,         ...
@@ -26,21 +47,30 @@ U = calc_radiation_pattern( ...
             pattern_step_x_m, ...
             pattern_step_y_m  ...
             );
+fprintf( '=============================\n'             );
 RP = ( abs(U) ).^2;
 X = pattern_min_x_m + (1:size(RP,1))*pattern_step_x_m;
 Y = pattern_min_y_m + (1:size(RP,2))*pattern_step_y_m;
 %______________________________________________________
 
-RP_mid =  RP(ceil(size(RP,2)/2),1:end);
+RP_mid   = RP(ceil(size(RP,2)/2),1:end);
 RP_width = fwhm(X, RP_mid);
 RP_peaks = findpeaks(RP_mid,'SortStr','descend');
-RP_max = max(RP(:));
-RP_fig = figure();
+RP_max   = max(RP(:));
+RP_fig   = figure();
 %___________________________________________________
 surfc( X, Y, RP/RP_max );
 title(['zone\_number = ', num2str(zone_number)]);
 xlabel('x, m');
 ylabel('y, m');
+fprintf( 'MAX              = %f\n', RP_max                     );
+fprintf( 'FWHM             = %f\n', RP_width                   );
+fprintf( 'SLL              = %f\n', RP_peaks(2)                );
+fprintf( 'SLL/MAX          = %f\n', RP_peaks(2)/RP_max         );
+fprintf( 'SUM              = %f\n', ( abs( sum(U(:)) ) ).^2    );
+fprintf( '=============================\n'                  );
+toc;
+% FIXME: anotation delete some spaces, idk what to do with it
 str = {
     ['ZONE\_NUMBER  = ',            num2str( zone_number             )],
     ['MAX                  = ',     num2str( RP_max                  )],
@@ -53,3 +83,4 @@ annotation(RP_fig,'textbox',...
     [0.701460829493088 0.389776357827476 0.283178187403994 0.327476038338658],...
     'String',str,...
     'FitBoxToText','on');
+diary off;
